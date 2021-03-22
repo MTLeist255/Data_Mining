1efbffc1f7e4dfa962a4d2ccec96f080aaffb87c
masonleist <mason.leist@utsa.edu>
2021-03-22 08:56:54 -0500

    Initial Commit

---
+++ Chapter_6/6.3.1)_GMM.py
@@ -0,0 +1,225 @@
+# figure 6.6/6.7)
+
+# FIGURE 6.6) GAUSSIAN MIXTURE MODEL (GMM)
+
+# A two-dimensional mixture of Gaussians for the stellar metallicity data. The left panel shows the number density of
+# stars as a function of two measures of their chemical composition: metallicity ([Fe/H]) and alpha-element abundance
+# ([alpha/Fe]). The right panel shows the density estimated using mixtures of Gaussians together with the positions
+# and covariances (2-sigma levels) of those Gaussians. The center panel compares the information criteria AIC and BIC
+# (see Sections 4.3.2 and 5.4.3).
+
+# Author: Jake VanderPlas
+# Edited by: Mason Leist
+# License: BSD
+#   The figure produced by this code is published in the textbook
+#   "Statistics, Data Mining, and Machine Learning in Astronomy" (2013)
+#   For more information, see http://astroML.github.com
+#   To report a bug or issue, use the following forum:
+#    https://groups.google.com/forum/#!forum/astroml-general
+
+from __future__ import print_function
+
+import numpy as np
+from matplotlib import pyplot as plt
+
+from sklearn.mixture import GaussianMixture
+
+from astroML.datasets import fetch_sdss_sspp
+from astroML.utils.decorators import pickle_results
+from astroML.plotting.tools import draw_ellipse
+from astroML.datasets import fetch_great_wall
+
+#----------------------------------------------------------------------
+# This function adjusts matplotlib settings for a uniform feel in the textbook.
+# Note that with usetex=True, fonts are rendered with LaTeX.  This may
+# result in an error if LaTeX is not installed on your system.  In that case,
+# you can set usetex to False.
+if "setup_text_plots" not in globals():
+    from astroML.plotting import setup_text_plots
+setup_text_plots(fontsize=8, usetex=False)
+
+#------------------------------------------------------------
+# Get the Segue Stellar Parameters Pipeline data
+data = fetch_sdss_sspp(cleaned=True)
+X = np.vstack([data['FeH'], data['alphFe']]).T
+
+# truncate dataset for speed
+X = X[::5]
+
+#------------------------------------------------------------
+# Compute GaussianMixture models & AIC/BIC
+N = np.arange(1, 10)
+
+# eq 6.17
+@pickle_results("GMM_metallicity.pkl")
+def compute_GaussianMixture(N, covariance_type='full', max_iter=1000):
+    models = [None for n in N]
+    for i in range(len(N)):
+        print(N[i])
+        models[i] = GaussianMixture(n_components=N[i], max_iter=max_iter,
+                                    covariance_type=covariance_type)
+        models[i].fit(X)
+    return models
+
+models = compute_GaussianMixture(N)
+
+# middle plot
+AIC = [m.aic(X) for m in models]
+BIC = [m.bic(X) for m in models]
+
+i_best = np.argmin(BIC)
+gmm_best = models[i_best]
+print("best fit converged:", gmm_best.converged_)
+print("BIC: n_components =  %i" % N[i_best])
+
+#------------------------------------------------------------
+# compute 2D density (left-most plot)
+FeH_bins = 51
+alphFe_bins = 51
+H, FeH_bins, alphFe_bins = np.histogram2d(data['FeH'], data['alphFe'],
+                                          (FeH_bins, alphFe_bins))
+
+Xgrid = np.array(list(map(np.ravel,
+                          np.meshgrid(0.5 * (FeH_bins[:-1]
+                                             + FeH_bins[1:]),
+                                      0.5 * (alphFe_bins[:-1]
+                                             + alphFe_bins[1:]))))).T
+log_dens = gmm_best.score_samples(Xgrid).reshape((51, 51))
+
+#------------------------------------------------------------
+# Plot the results
+fig = plt.figure(figsize=(5, 1.66))
+fig.subplots_adjust(wspace=0.45,
+                    bottom=0.25, top=0.9,
+                    left=0.1, right=0.97)
+
+# plot density
+# a HESS diagram (essentially a 2D histogram) of the [Fe/H] vs [a/Fe] metallicity for a subset of the SEGUE stellar
+# parameters data -> shows 2 distinct clusters in matallicity
+
+ax = fig.add_subplot(131)
+ax.imshow(H.T, origin='lower', interpolation='nearest', aspect='auto',
+          extent=[FeH_bins[0], FeH_bins[-1],
+                  alphFe_bins[0], alphFe_bins[-1]],
+          cmap=plt.cm.binary)
+ax.set_xlabel(r'$\rm [Fe/H]$')
+ax.set_ylabel(r'$\rm [\alpha/Fe]$')
+ax.xaxis.set_major_locator(plt.MultipleLocator(0.3))
+ax.set_xlim(-1.101, 0.101)
+ax.text(0.93, 0.93, "Input",
+        va='top', ha='right', transform=ax.transAxes)
+
+# plot AIC/BIC
+#
+# shows both flavor models w/ 5> components, this is due to the fact that components exist in the background and the background
+# is such that a 2-component model is insufficient to fully describe the data.
+#
+# AIC (Akaike Information Criterion): a simple approach based on an asympototic approximation; the preffered approach
+# to model comparison for the cross-validation, which is based on only the finite data at hand rather than approximations
+# based on infinite data (sec 4.3.2)
+#
+# BIC (Bayesian Information Criterion): attempts to simplify the computation of the odds ratio by making certain assumptions about the likelihood. Easier to
+# # compute and, like the AIC, is based on the max value of the data likelihood (sec 5.4.3)
+
+# When two models are compared, their BIC(or AIC) are compared analogously to the odds ratio, that is, the model with
+# the smaller value wins
+ax = fig.add_subplot(132)
+ax.plot(N, AIC, '-k', label='AIC')
+ax.plot(N, BIC, ':k', label='BIC')
+ax.legend(loc=1)
+ax.set_xlabel('N components')
+plt.setp(ax.get_yticklabels(), fontsize=7)
+plt.title('Figure 6.6) 2D mix of Gaussians for the stellar metallicity data')
+
+# plot best configurations for AIC and BIC
+#
+# Following the BIC, we get N=5. Here is the common confusion w/ GMM: the fact that the info criteria (BIC/AIC) prefer an
+# N-component peak does not necissarily mean that there are N-components. If the clusters in the input data are not near
+# Gaussian, or if there is a strong background, the number of Gaussian components in the mix will not generally correspond
+# to the number of clusters in the data
+ax = fig.add_subplot(133)
+ax.imshow(np.exp(log_dens),
+          origin='lower', interpolation='nearest', aspect='auto',
+          extent=[FeH_bins[0], FeH_bins[-1],
+                  alphFe_bins[0], alphFe_bins[-1]],
+          cmap=plt.cm.binary)
+
+ax.scatter(gmm_best.means_[:, 0], gmm_best.means_[:, 1], c='w')
+for mu, C, w in zip(gmm_best.means_, gmm_best.covariances_, gmm_best.weights_):
+    draw_ellipse(mu, C, scales=[1.5], ax=ax, fc='none', ec='k')
+
+ax.text(0.93, 0.93, "Converged",
+        va='top', ha='right', transform=ax.transAxes)
+
+ax.set_xlim(-1.101, 0.101)
+ax.set_ylim(alphFe_bins[0], alphFe_bins[-1])
+ax.xaxis.set_major_locator(plt.MultipleLocator(0.3))
+ax.set_xlabel(r'$\rm [Fe/H]$')
+ax.set_ylabel(r'$\rm [\alpha/Fe]$')
+plt.show()
+
+# FIGURE 6.7): A two-dimensional mixture of 100 Gaussians (bottom) used to estimate the number density distribution of
+# galaxies within the SDSS Great Wall (top). Compare to figures 6.3 and 6.4, where the density for the same
+# distribution is computed using both kernel density and nearest-neighbor-based estimates.
+
+# While the underlying density representation is consistent with the distribution of galaxies and the positions of the
+# Gaussians themselves correlate with the structure, there is not a one-to-one mapping between the Gaussians and the
+# positions of clusters within the data. For these reasons, GMM are often more appropriate when used as a density
+# estimator as oppossed to cluster identification.
+#------------------------------------------------------------
+# load great wall data
+X = fetch_great_wall()
+
+
+#------------------------------------------------------------
+# Create a function which will save the results to a pickle file
+#  for large number of clusters, computation will take a long time!
+@pickle_results('great_wall_GMM.pkl')
+def compute_GMM(n_clusters, max_iter=1000, tol=3, covariance_type='full'):
+    clf = GaussianMixture(n_clusters, covariance_type=covariance_type,
+                          max_iter=max_iter, tol=tol, random_state=0)
+    clf.fit(X)
+    print("converged:", clf.converged_)
+    return clf
+
+#------------------------------------------------------------
+# Compute a grid on which to evaluate the result
+Nx = 100
+Ny = 250
+xmin, xmax = (-375, -175)
+ymin, ymax = (-300, 200)
+
+Xgrid = np.vstack(map(np.ravel, np.meshgrid(np.linspace(xmin, xmax, Nx),
+                                            np.linspace(ymin, ymax, Ny)))).T
+
+#------------------------------------------------------------
+# Compute the results
+#
+# we'll use 100 clusters.  In practice, one should cross-validate
+# with AIC and BIC to settle on the correct number of clusters.
+#   INCREASE/DECREASE clustering
+clf = compute_GMM(n_clusters=100)
+log_dens = clf.score_samples(Xgrid).reshape(Ny, Nx)
+
+#------------------------------------------------------------
+# Plot the results
+fig = plt.figure(figsize=(5, 3.75))
+fig.subplots_adjust(hspace=0, left=0.08, right=0.95, bottom=0.13, top=0.9)
+
+ax = fig.add_subplot(211, aspect='equal')
+ax.scatter(X[:, 1], X[:, 0], s=1, lw=0, c='k')
+
+ax.set_xlim(ymin, ymax)
+ax.set_ylim(xmin, xmax)
+
+ax.xaxis.set_major_formatter(plt.NullFormatter())
+plt.ylabel(r'$x\ {\rm (Mpc)}$')
+plt.title('Figure 6.7) SDSS Great Wall Gaussian Mixture')
+ax = fig.add_subplot(212, aspect='equal')
+ax.imshow(np.exp(log_dens.T), origin='lower', cmap=plt.cm.binary,
+          extent=[ymin, ymax, xmin, xmax])
+ax.set_xlabel(r'$y\ {\rm (Mpc)}$')
+ax.set_ylabel(r'$x\ {\rm (Mpc)}$')
+# Un-comment to display plot
+#plt.show()
+
---
+++ Chapter_6/Gonzalez_Algorithm.py
@@ -0,0 +1,163 @@
+# Here is a practice set of data to demonstrate the Gonzalez ALgorithm in 6.4.3). Once the base code is running,
+# we'll adapt this code to run with the SDSS data from Figure 6.6) and 6.13). Code referrenced from
+# https://www.kaggle.com/barelydedicated/gonzalez-algorithm?scriptVersionId=2656948
+
+from __future__ import print_function
+
+import numpy as np
+import pandas as pd
+from matplotlib import pyplot as plt
+from matplotlib.patches import Ellipse
+from scipy.stats import norm
+from matplotlib import pyplot as plt
+from scipy.spatial import distance
+import seaborn as sns
+import matplotlib as mpl
+import math
+import os
+from astroML.datasets import fetch_sdss_sspp
+
+np.random.seed(0)
+data = fetch_sdss_sspp(cleaned=True)
+
+# cut out some additional strange outliers-> DEMO CHANGES
+#data = data[~((data['alphFe'] > 0.4) & (data['FeH'] > -0.3))]
+
+X = np.double(np.vstack([data['FeH'], data['alphFe']]).T)
+
+# Gonzalez algorithm math ->
+def gonzalez(data, cluster_num, technique = 'max'):
+    clusters = []
+    clusters.append(data[0]) # let us assign the first cluster point to be first point of the data
+    while len(clusters) != cluster_num:
+        if technique == 'max':
+            clusters.append(max_dist(data, clusters))
+        if technique == 'norm':
+            clusters.append(norm_dist(data, clusters))
+        # we add the furthest point from ALL current clusters
+    return (clusters)
+
+# COMMENT THIS -> Explain coding in order (max-dist undefined up until here)
+cluster_points = gonzalez(X, 3)
+A = np.array(cluster_points)
+print('test', A)
+
+def max_dist(data, clusters):
+    distances = np.zeros(len(data)) # we will keep a cumulative distance measure for all points
+    for cluster_id, cluster in enumerate(clusters):
+        for point_id, point in enumerate(data):
+            if distance.euclidean(point,cluster) == 0.0:
+                distances[point_id] = -math.inf # this point is already a cluster (obselete)
+            if not math.isinf(distances[point_id]):
+                # if a point is not obselete, then we add the distance to its specific bin
+                distances[point_id] = distances[point_id] + distance.euclidean(point,cluster)
+                # return the point which is furthest away from all the other clusters
+    return data[np.argmax(distances)]
+
+# cluster point works here
+cluster_points = gonzalez(X, 3)
+A = np.array(cluster_points)
+print('test', A)
+
+cluster_points = gonzalez(X, 3)
+print('Cluster Centeroids (N = 3):', cluster_points)
+
+cluster_distance = np.full(len(X), np.inf)
+for point_idx, point in enumerate(X):
+    for cluster_idx, cluster_point in enumerate(cluster_points):
+        if cluster_distance[point_idx] == math.inf:
+            cluster_distance[point_idx] = distance.euclidean(point,cluster_point)
+            continue
+        if distance.euclidean(point,cluster_point) < cluster_distance[point_idx]:
+            cluster_distance[point_idx] = distance.euclidean(point,cluster_point)
+print('3-center cost:', np.max(cluster_distance))
+
+cost = math.sqrt(np.sum(cluster_distance**2, axis=0) /len(X))
+print('3-means cost:', cost)
+
+plt.scatter(data['FeH'],data['alphFe'])
+for index, point in enumerate(cluster_points):
+    if index != 0: # these points are generated
+        plt.scatter(point[0],point[1], marker='*', c='red', s=50)
+    if index == 0: # this is our ifrst point, which was picked staticly
+        plt.scatter(point[0],point[1], marker='*', c='orange', s=50)
+
+cluster_points = gonzalez(X, 4)
+print('Cluster Centeroids (N = 4):', cluster_points)
+
+# plt.scatter(data['FeH'],data['alphFe'])
+# for index, point in enumerate(cluster_points):
+#     if index is not 0: # these points are generated
+#         plt.scatter(point[0],point[1], marker='*', c='red', s=50)
+#     if index is 0: # this is our ifrst point, which was picked staticly
+#         plt.scatter(point[0],point[1], marker='*', c='orange', s=50)
+
+def norm_dist(data, clusters):
+    distances = np.zeros(len(data)) # we will keep a cumulative distance measure for all points
+    for point_id, point in enumerate(data):
+        for cluster_id, cluster in enumerate(clusters):
+            if distance.euclidean(point,cluster) == 0.0:
+                distances[point_id] = -math.inf # this point is already a cluster (obselete)
+            if not math.isinf(distances[point_id]):
+                # if a point is not obselete, then we add the distance to its specific bin
+                distances[point_id] = distances[point_id] + math.pow(distance.euclidean(point,cluster),2)
+                # return the point which is furthest away from all the other clusters
+    for distance_id, current_distance in enumerate(distances):
+        if not math.isinf(current_distance):
+            distances[distance_id] = math.sqrt(current_distance/len(data))
+    return data[np.argmax(distances)]
+#
+# cluster_points = gonzalez(X, 3,'norm')
+# print('Cluster Centeroids (N = 3):', cluster_points)
+#
+# # plt.scatter(data['FeH'],data['alphFe'])
+# # for index, point in enumerate(cluster_points):
+# #     if index is not 0: # these points are generated
+# #         plt.scatter(point[0],point[1], marker='*', c='red', s=50)
+# #     if index is 0: # this is our ifrst point, which was picked staticly
+# #         plt.scatter(point[0],point[1], marker='*', c='orange', s=50)
+#
+# # Visualize the results
+# H, FeH_bins, alphFe_bins = np.histogram2d(data['FeH'], data['alphFe'], 50)
+# fig = plt.figure(figsize=(5, 5))
+# ax = fig.add_subplot()
+#
+# # plot density
+# ax = plt.axes()
+# ax.imshow(H.T, origin='lower', interpolation='nearest', aspect='auto',
+#           extent=[FeH_bins[0], FeH_bins[-1],
+#                   alphFe_bins[0], alphFe_bins[-1]],
+#           cmap=plt.cm.binary)
+#
+# # plot cluster centers
+# cluster_points = gonzalez(X, 4)
+# #cluster_centers = scaler.inverse_transform(clf.cluster_centers_)
+# ax.scatter(cluster_points[:][0], cluster_points[:][1],
+#            s=40, c='w', edgecolors='k')
+#
+# # plot cluster boundaries
+# FeH_centers = 0.5 * (FeH_bins[1:] + FeH_bins[:-1])
+# alphFe_centers = 0.5 * (alphFe_bins[1:] + alphFe_bins[:-1])
+#
+# Xgrid = np.meshgrid(FeH_centers, alphFe_centers)
+# Xgrid = np.array(Xgrid).reshape((2, 50 * 50)).T
+#
+# #H = clf.predict(scaler.transform(Xgrid)).reshape((50, 50))
+#
+# for i in range(cluster_points):
+#     Hcp = H.copy()
+#     flag = (Hcp == i)
+#     Hcp[flag] = 1
+#     Hcp[~flag] = 0
+#
+#     ax.contour(FeH_centers, alphFe_centers, Hcp, [-0.5, 0.5],
+#                linewidths=1, colors='k')
+#
+# ax.xaxis.set_major_locator(plt.MultipleLocator(0.3))
+# ax.set_xlim(-1.101, 0.101)
+# ax.set_ylim(alphFe_bins[0], alphFe_bins[-1])
+#
+# ax.set_xlabel(r'$\rm [Fe/H]$')
+# ax.set_ylabel(r'$\rm [\alpha/Fe]$')
+# plt.title('Figure 6.13) K-means analysis of the stellar\n metallicity data from Fig 6.6)')
+# plt.show()
